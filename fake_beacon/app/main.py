f98fc31df10208d5aaf8d137ffc21c756be88763
Jonathan Dursi <jonathan@dursi.ca>
2020-08-22 12:51:45 -0400

    PEP-8

--- fake_beacon/app/main.py
+++ fake_beacon/app/main.py
@@ -1,6 +1,6 @@
 """
 This toy service implements the role of the beacon in this deployment -
-accessing the OAuth2/OIDC service for authentication and querying the 
+accessing the OAuth2/OIDC service for authentication and querying the
 permissions server for authorization.
 """
 from typing import Optional
@@ -22,15 +22,19 @@ permissions_server = os.getenv("PERMISSIONS_SERVER", "https://opa:8181/v1/data/p
 
 @app.get("/login")
 def get_token(username: Optional[str] = "", password: Optional[str] = ""):
+    """
+    Perform user credentials flow with OIDC and return token for testing purposes
+    """
     payload = {'grant_type': 'password',
                'username': username,
                'password': password,
                'redirect_uri': "http://fake_beacon:8000/auth/oidc"}
 
     if rootCA:
-        response = requests.post(f"{idp}/token", data=payload, auth=(client_id, client_secret), verify=rootCA)
+        response = requests.post(f"{idp}/token", auth=(client_id, client_secret), verify=rootCA,
+                                 data=payload)
     else:
-        response = requests.post(f"{idp}/token", data=payload, auth=(client_id, client_secret))
+        response = requests.post(f"{idp}/token", auth=(client_id, client_secret), data=payload)
 
     if response.status_code != 200:
         raise HTTPException(status_code=response.status_code)
@@ -45,11 +49,14 @@ def get_token(username: Optional[str] = "", password: Optional[str] = ""):
 
 @app.get("/permissions")
 def get_permissions(token: Optional[str] = ""):
+    """
+    Given passed token, look up permissions for that user
+    """
     if not token:
         raise HTTPException(status_code=401, detail="Not logged in")
 
-    response = requests.post(permissions_server, 
+    response = requests.post(permissions_server,
                              json={"input": {"method": "GET", "path": ["beacon"], "token": token}},
                              verify=rootCA)
-    
+
     return response.json()
