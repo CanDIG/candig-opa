06f6a07b193247d0d6df941b816fa888bfe1a6e1
Jonathan Dursi <jonathan@dursi.ca>
2021-05-30 19:52:26 -0400

    Add SAN to certificates

--- generate-certs.sh
+++ generate-certs.sh
@@ -10,16 +10,16 @@ openssl req -x509 -new -nodes -key rootCA.key -subj '/C=CA/ST=Ontario/O=Demo/CN=
 
 openssl genrsa -out oidc/tls.key 2048
 openssl req -new -sha256 -key oidc/tls.key -subj '/C=CA/ST=Ontario/O=Demo/CN=oidc' -out oidc/tls.csr
-openssl x509 -req -in oidc/tls.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out oidc/tls.crt -days 500 -sha256
+openssl x509 -req -in oidc/tls.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out oidc/tls.crt -days 500 -sha256 -extfile oidc/oidc.v3.ext
 
 # generate OPA cert
 
 openssl genrsa -out permissions_engine/tls.key 2048
 openssl req -new -sha256 -key permissions_engine/tls.key -subj '/C=CA/ST=Ontario/O=Demo/CN=opa' -out permissions_engine/tls.csr
-openssl x509 -req -in permissions_engine/tls.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out permissions_engine/tls.crt -days 500 -sha256
+openssl x509 -req -in permissions_engine/tls.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out permissions_engine/tls.crt -days 500 -sha256 -extfile permissions_engine/opa.v3.ext
 
 # generate shim cert
 
 openssl genrsa -out permissions_shim/tls.key 2048
 openssl req -new -sha256 -key permissions_shim/tls.key -subj '/C=CA/ST=Ontario/O=Demo/CN=shim' -out permissions_shim/tls.csr
-openssl x509 -req -in permissions_shim/tls.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out permissions_shim/tls.crt -days 500 -sha256
+openssl x509 -req -in permissions_shim/tls.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out permissions_shim/tls.crt -days 500 -sha256 -extfile permissions_shim/shim.v3.ext
