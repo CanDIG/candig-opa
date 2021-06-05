599c6375e7d44f747f8c4fc6f22e63843f67d4e6
Jonathan Dursi <jonathan@dursi.ca>
2021-06-05 17:04:14 -0400

    token helper plus docstrings

--- tests/test_authx.py
+++ tests/test_authx.py
@@ -1,6 +1,5 @@
 import pytest
 import requests
-import sys
 """
 This test suite will cover the manual tests in README.md, ensuring that
 authorization happens correctly
@@ -17,17 +16,10 @@ LOGIN=f"{BEACON_URL}/login"
 PERMISSIONS=f"{BEACON_URL}/permissions"
 PERMISSIONS_COUNT=f"{BEACON_URL}/permissions_count"
 
-@pytest.fixture
-def user1_token():
-    """
-    Return the token for user1
-    """
-    user, passwd = "user1", "pass1"
+def helper_get_user_token(username, password):
     token_field = "access_token"
 
-    print(f"{LOGIN}?username={user}&password={passwd}", file=sys.stderr)
-    response = requests.get(f"{LOGIN}?username={user}&password={passwd}")
-
+    response = requests.get(f"{LOGIN}?username={username}&password={password}")
     assert response.status_code == 200
 
     body = response.json()
@@ -36,7 +28,7 @@ def user1_token():
 
 
 def helper_get_permissions(token, url):
-    response = requests.get(f"{url}?token=${user1_token}")
+    response = requests.get(f"{url}?token={token}")
     assert response.status_code == 200
 
     body = response.json()
@@ -44,13 +36,25 @@ def helper_get_permissions(token, url):
     return body["datasets"]
 
 
+@pytest.fixture
+def user1_token():
+    """
+    Return the token for user1
+    """
+    return helper_get_user_token("user1", "pass1")
+
+
 def test_user1_controlled_access(user1_token):
+    """"
+    Make sure user1 has access to controlled4
+    """
     datasets = helper_get_permissions(user1_token, PERMISSIONS)
     assert "controlled4" in datasets
 
 
 def test_user1_registered_access(user1_token):
+    """
+    User1, being a trusted researcher, should have acess to registered3
+    """
     datasets = helper_get_permissions(user1_token, PERMISSIONS)
     assert "registered3" in datasets
-
-
