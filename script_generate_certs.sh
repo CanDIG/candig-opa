47bcbf3b4a3a08e03d738f826c817ef39f0d6be6
Jonathan Dursi <jonathan@dursi.ca>
2021-06-05 17:22:29 -0400

    Add travis run

---
+++ script_generate_certs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/expect
+
+set timeout 20
+set password [lindex $argv 0]
+
+eval spawn ./generate-certs.sh
+expect "rootCA.key:"
+send "$password\r";
+expect "rootCA.key:"
+send "$password\r";
+expect "rootCA.key:"
+send "$password\r";
+expect "rootCA.key:"
+send "$password\r";
+expect "rootCA.key:"
+send "$password\r";
+expect "rootCA.key:"
+send "$password\r";
+interact
