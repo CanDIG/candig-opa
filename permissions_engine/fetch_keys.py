11c130aaaf95fa21733dc107bd3da8ea419aaf31
Daisie Huang <daisieh@gmail.com>
2023-09-21 15:51:19 -0700

    only handle our own IDP in fetch_keys.py

--- permissions_engine/fetch_keys.py
+++ permissions_engine/fetch_keys.py
@@ -2,16 +2,14 @@ import requests
 import json
 import os
 
-IDP = os.getenv("IDP")
+MY_KEYCLOAK_REALM = os.getenv("KEYCLOAK_REALM_URL")
 
 data = dict()
 data["keys"] = []
-issuers = [IDP]
 
-for external in issuers:
-    response = requests.get(external + "/.well-known/openid-configuration")
-    json_data = json.loads(response.text)
-    jwks = requests.get(json_data["jwks_uri"]).text
-    data["keys"].append({"iss": external, "cert": jwks})
+response = requests.get(MY_KEYCLOAK_REALM + "/.well-known/openid-configuration")
+json_data = json.loads(response.text)
+jwks = requests.get(json_data["jwks_uri"]).text
+data["keys"].append({"iss": MY_KEYCLOAK_REALM, "cert": jwks})
 with open('/app/data.json', 'w') as f:
     json.dump(data, f)
