3dd9376bd9d5b2361e7c62e209c5155948d2ce59
Daisie Huang <daisieh@gmail.com>
2023-12-21 12:01:24 -0800

    on initial setup, store jsons in vault

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,9 +15,33 @@ if [[ -f "/app/initial_setup" ]]; then
     echo "initializing idp"
     python3 /app/permissions_engine/initialize_idp.py
 
+    # install base jsons at vault
     token=$(bash get_vault_store_token.sh)
+    echo "storing data.json"
+    curl -X "PUT" "${VAULT_URL}/v1/opa/data" \
+     -H "X-Vault-Token: ${token}" \
+     -H 'Content-Type: application/json; charset=utf-8' \
+     -d @/app/data.json
+
+    echo "storing access.json"
+    curl -X "PUT" "${VAULT_URL}/v1/opa/access" \
+     -H "X-Vault-Token: ${token}" \
+     -H 'Content-Type: application/json; charset=utf-8' \
+     -d @/app/permissions_engine/access.json
+
+    echo "storing paths.json"
+    curl -X "PUT" "${VAULT_URL}/v1/opa/paths" \
+     -H "X-Vault-Token: ${token}" \
+     -H 'Content-Type: application/json; charset=utf-8' \
+     -d @/app/permissions_engine/paths.json
+
     rm /app/initial_setup
 fi
+
+token=$(bash get_vault_store_token.sh)
+echo $(curl "${VAULT_URL}/v1/opa/data" -H "X-Vault-Token: ${token}")
+echo $(curl "${VAULT_URL}/v1/opa/access" -H "X-Vault-Token: ${token}")
+echo $(curl "${VAULT_URL}/v1/opa/paths" -H "X-Vault-Token: ${token}")
 
 while [ 0 -eq 0 ]
 do
