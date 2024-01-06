00d8e70f38a1b853f7b898f5c48b533f5b53619e
Daisie Huang <daisieh@gmail.com>
2024-01-05 16:25:35 -0800

    Update get_vault_store_token.py

--- get_vault_store_token.py
+++ get_vault_store_token.py
@@ -7,14 +7,16 @@ import requests
 
 # get the token for the opa store
 try:
-    opa_token = get_vault_token_for_service("opa")
-    headers = {
-        "X-Opa": os.getenv("OPA_ROOT_TOKEN"),
-        "Content-Type": "application/json; charset=utf-8"
-    }
-    json = {
-        "token": opa_token
-    }
-    response = requests.put(url=f"{os.getenv('OPA_URL')}/v1/data/store_token", headers=headers, json=json)
-except:
+    with open("/run/secrets/opa-root-token") as f:
+        OPA_ROOT_TOKEN = f.read().strip()
+        opa_token = get_vault_token_for_service("opa")
+        headers = {
+            "X-Opa": OPA_ROOT_TOKEN,
+            "Content-Type": "application/json; charset=utf-8"
+        }
+        payload = f"{{\"token\": \"{opa_token}\"}}"
+        response = requests.put(url=f"{os.getenv('OPA_URL')}/v1/data/store_token", headers=headers, data=payload)
+        print(response.text)
+except Exception as e:
+    print(str(e))
     sys.exit(1)
